bb6a3024b3ba9dff64af912e3cefd8bec04acc1c
LARocca-ReCod <yaeriks@yandex.ru>
2016-08-23 23:41:56 +0300

    Update rules.cfg.php

--- RCM/cfg/rules.cfg.php
+++ RCM/cfg/rules.cfg.php
@@ -1,5 +1,6 @@
 <?php 
 
+// TOTAL 5 Messages
 
 $messagenm1 = '^1Do not use ^3CHEATS^1, or ^3BAN!';
 
@@ -7,5 +8,8 @@ $messagenm2 = '^1FS / FR / Weapon32 / Glitches / Fakes / Flood / = ban!';
 
 $messagenm3 = '^1Use ^7'.$ixz.'report ^1for cheaters report!';
 
+// $messagenm4 = 'Message 4';
+
+// $messagenm5 = 'Message 5';
 
 ?>
