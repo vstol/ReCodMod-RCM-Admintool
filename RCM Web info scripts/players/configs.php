593653984ea7a85835034993acb543dfd558f77d
Erick <yaeriks@yandex.ru>
2016-11-29 20:38:44 +0200

    Add files via upload

---
+++ RCM Web info scripts/players/configs.php
@@ -0,0 +1,13 @@
+<?php
+//RCM database path
+ $dbsqlite3  = '/media/Windows/Game_Servers/RCM/COD1_1_4_RO_SQL3/ReCodMod/databases/db4.sqlite';
+//server name
+$servername = '^3[CRACKED]Rifles^7|recod.ru 1.4';
+//color in top
+$ccc        = '^5';
+//in top when player killed more than this number
+$top_limit  = '500';
+//number players in the page
+$number_msg_at_page = 10; 
+//All system cache for faster and stable RCM database work // cache time in minutes. make more 3 minutes
+$cachetime = 5; 
