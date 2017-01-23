193771bd88feda9e21b182faffeca69090306f24
Erick <yaeriks@yandex.ru>
2017-01-23 22:22:36 +0200

    Update log_reader_stats_updater.php

--- RCM/ReCodMod/plugins/log_reader_stats_updater.php
+++ RCM/ReCodMod/plugins/log_reader_stats_updater.php
@@ -665,15 +665,15 @@ fclose($connect);
                     if ((strpos($byweapon, 'grenade_') !== false) && ($iddeath != $guidcc))	
                       $db3->exec("UPDATE x_db_play_stats SET s_player='{$kill}',s_grenade=s_grenade +1 WHERE s_guid='{$guidcc}'");
                     if ((strpos($hitlock, 'head') !== false) && ($iddeath != $guidcc))
-                      $db3->exec("UPDATE x_db_play_stats SET s_player='{$kill}'s_heads=s_heads +1 WHERE s_guid='{$guidcc}'");
+                      $db3->exec("UPDATE x_db_play_stats SET s_player='{$kill}',s_heads=s_heads +1 WHERE s_guid='{$guidcc}'");
                     if ((strpos($modkll, 'MOD_MELEE') !== false) && ($iddeath != $guidcc))
-                      $db3->exec("UPDATE x_db_play_stats SET s_player='{$kill}'s_melle=s_melle +1 WHERE s_guid='{$guidcc}'");
+                      $db3->exec("UPDATE x_db_play_stats SET s_player='{$kill}',s_melle=s_melle +1 WHERE s_guid='{$guidcc}'");
                     if (($modkll == 'MOD_SUICIDE') && ($iddeath == $guidcc))
                       echo 'suicide';
                     if (($modkll != 'MOD_SUICIDE') && ($iddeath != $guidcc)    
 						|| (($modkll == 'MOD_SUICIDE') && ($iddeath != $guidcc) && ($byweapon != 'none'))
 					    || (($modkll == 'MOD_EXPLOSIVE') && ($iddeath != $guidcc) && ($byweapon == 'none')))
-                      $db3->exec("UPDATE x_db_play_stats SET s_player='{$kill}'s_kills=s_kills +1 WHERE s_guid='{$guidcc}'");
+                      $db3->exec("UPDATE x_db_play_stats SET s_player='{$kill}',s_kills=s_kills +1 WHERE s_guid='{$guidcc}'");
 
                 ////////////////AUTO SCREENSHOTS
 				$cron_timeq=filemtime($cpath."ReCodMod/x_cron/cron_y");        
